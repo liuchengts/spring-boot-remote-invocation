6ef091853b1c333ad304c0a084d8aae497f8f72d
lc <790809568@qq.com>
2018-11-17 14:43:41 +0800

    去掉心跳日志

--- spring-boot-remote-invocation-starter/src/main/java/org/remote/invocation/starter/network/BaseHandler.java
+++ spring-boot-remote-invocation-starter/src/main/java/org/remote/invocation/starter/network/BaseHandler.java
@@ -79,7 +79,7 @@ public abstract class BaseHandler extends ChannelInboundHandlerAdapter {
             String m = (String) msg;
             if (m.startsWith(HEARTBEAT)) {
                 Long time = Long.valueOf(m.replace(HEARTBEAT, ""));
-                log.info("[" + name + "]心跳连接维持 " + (System.currentTimeMillis() - time));
+//                log.info("[" + name + "]心跳连接维持 " + (System.currentTimeMillis() - time));
             } else if (m.startsWith(SEIZELEADER)) {
                 //请求回发当前服务器路由信息
                 pushAllRouteCache();
